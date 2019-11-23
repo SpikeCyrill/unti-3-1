73d9c3f58481721689eeefa9fe979d2c9ba39c45
svetomsk <erere>
2019-11-23 16:23:45 +0300

    words correction

--- src/ru/kolosov/unicorn/Application.java
+++ src/ru/kolosov/unicorn/Application.java
@@ -3,7 +3,7 @@ package ru.kolosov.unicorn;
 public class Application {
 
 	public static void main(String[] args) {
-        System.out.println("Hellow world!");
+        System.out.println("Hello world!");
 	}
 
 }
